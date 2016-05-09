a8b6c36fc0327f9b691a74d2d1b26729e5b03764
lfalcantar <lfalcantar@miners.utep.edu>
2016-05-09 16:33:22 -0600

    Update Repository

--- java/nQueen/Runner.java
+++ java/nQueen/Runner.java
@@ -1,7 +1,7 @@
 public class Runner{
    public static void main(String [] args){
-	  
-    int size = 3; /*this can be change to any number >= 4*/
+	  System.out.println("Running");
+    int size = 8; /*this can be change to any number >= 4*/
     Board board = new Board(size);
     NQueens_Problem.solveNQueen(board,0); 
     
