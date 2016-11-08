eee7d621963db59711774b3ac6ce12dc22c22670
lfalcantar <lfalcantar@miners.utep.edu>
2016-11-08 16:35:52 -0700

    update file names and structure

---
+++ java-projects/algorithms/nQueen/Board.java
@@ -0,0 +1,109 @@
+public class Board {
+	private char[][] board;
+	private int numberQueens;
+	public int size;
+
+	/** 
+	 Customize constructor that will take size:int as the board length, 
+	 it will also initialize variables and place asterics in the boar '*' */	
+	public Board(int size) {
+		this.size = size;
+		this.board = new char[size][size];
+		this.numberQueens = 0;
+		initBoard();
+	}
+	
+	
+	public char[][] getBoard() {
+		return this.board;
+	}
+
+	public void initBoard() {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				board[i][j] = '*';
+			}
+		}
+	}
+
+	public boolean safeToPlace(int row, int col) {
+		return checkColum(col) && checkLeftDiagonal(row, col) && checkRightDiagonal(row, col) ? true : false;
+	}
+
+	public boolean checkColum(int col) {
+		for (int j = 0; j < board.length; j++) {
+			if (board[j][col] == 'Q') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean checkLeftDiagonal(int row, int column) {
+		/* Checking the down of the left diagonal */
+		int i = row, j = column;
+		while (checkIndices(i, j)) {
+			if (board[i++][j--] == 'Q') {
+				return false;
+			}
+		}
+		/* Checking the up of the left diagonal */
+		i = row;
+		j = column;
+		while (checkIndices(i, j)) {
+			if (board[i--][j++] == 'Q') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean checkRightDiagonal(int row, int column) {
+		/* Checking the down of the rigth diagonal */
+		int i = row, j = column;
+		while (checkIndices(i, j)) {
+			if (board[i++][j++] == 'Q') {
+				return false;
+			}
+		}
+		/* Checking the up of the rigth diagonal */
+		i = row;
+		j = column;
+		while (checkIndices(i,j)) {
+			if (board[i--][j--] == 'Q') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void addChar(int row, int column, char c) {
+		this.board[row][column] = c;
+		this.numberQueens++;
+	}
+	
+	/*make sure the indices in the loops are between the board boundaries*/
+	public boolean checkIndices(int i, int j){
+		return i < size && j < size && j >= 0 && i >= 0?true:false;
+	}
+
+	public void deleteChar(int row, int column, char c) {
+		this.board[row][column] = c;
+		this.numberQueens--;
+	}
+
+	public int getNumberQueens() {
+		return numberQueens;
+	}
+
+	public void printBoard() {
+		for (char r[] : board) {
+			for (char c : r) {
+				System.out.print(" " + c);
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
