d7fa2ccd65e550edfcfe3b0e680641dd5d6e1a67
Luis F Alcantar[xzbq38] <luis.alcantar@gm.com>
2018-12-30 16:30:36 -0700

    Update develop branch and setup project for angular app

--- ionics/ionicNews/src/app/services/news.service.ts
+++ ionics/ionicNews/src/app/services/news.service.ts
@@ -1,20 +1,41 @@
+import { IonicModule } from '@ionic/angular';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { tap } from 'rxjs/operators';
 
 const apiKey = environment.newsApiKey;
 const apiUrl = environment.newsApiUrl;
-
 const params = new HttpParams().set('apikey', apiKey);
 @Injectable({
   providedIn: 'root'
 })
 
 export class NewsService {
+  loading;
+  constructor(
+    private http: HttpClient,
+    public loadingController: LoadingController
+  ) {}
 
-  constructor(private http: HttpClient) { }
+  async showLoading() {
+    this.loading = await this.loadingController.create({
+      duration: 2000
+    });
+
+    return await this.loading.present();
+  }
 
   getData(url) {
-    return this.http.get(`${apiUrl}/${url}`, {params});
+    this.showLoading();
+    return this.http.get(`${apiUrl}/${url}`, { params }).pipe(
+      tap(value => {
+        if (this.loading) {
+          this.loading.dismiss();
+        }
+        console.log(value);
+      })
+    );
   }
 }
