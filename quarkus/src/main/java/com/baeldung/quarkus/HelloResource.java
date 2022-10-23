cd47e77d7b67d47773d034b5296c81a5ac02027b
Luis Alcantar <luisfalcantar.gm@gmail.com>
2022-10-23 09:41:05 -0700

    quarkus: init hello world endpoint

---
+++ quarkus/src/main/java/com/baeldung/quarkus/HelloResource.java
@@ -0,0 +1,17 @@
+package com.baeldung.quarkus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "hello";
+    }
+
+}
