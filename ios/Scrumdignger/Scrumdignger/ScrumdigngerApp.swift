fbe7f9f4017117f4006df0fa7990759f3037e2bd
Luis Alcantar <luis.herrera20034@gmail.com>
2022-12-31 09:42:24 -0700

    init: scrumdinger project

---
+++ ios/Scrumdignger/Scrumdignger/ScrumdigngerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdigngerApp.swift
+//  Scrumdignger
+//
+//  Created by Luis Acantar on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdigngerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
